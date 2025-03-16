985a455ecd3ff26f38bd5360e34a9ad20aeb0428
Alexis Chân Gridel <alexis.gridel@pm.me>
2025-03-16 20:17:19 +0100

    fix: metro config more minimalistic

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
-const { fileURLToPath } = require('url');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: "./global.css" });
